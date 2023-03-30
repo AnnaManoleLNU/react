86e30104ea6d6a313ead20d2177f478d4916fd1b
Anna Manole <am224wd@student.lnu.se>
2023-03-30 13:59:18 +0200

    some fetching

--- my-first-react-app/src/Home.js
+++ my-first-react-app/src/Home.js
@@ -2,39 +2,31 @@ import { useState, useEffect } from "react"
 import BlogList from "./BlogList"
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
-
-  const [name, setName] = useState('mario') // mario is the initial value
-
-  const handleDelete = (id) => { 
-    const newBlogs = blogs.filter(blog => blog.id !== id)
-    setBlogs(newBlogs)
-  }
+  const [blogs, setBlogs] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
 
   // use data from the server or communicate with the server in here
   // runs for every render
   // be careful not to create an infinite loop
   // use to fetch data from the server
   useEffect(() => {
-    console.log('use effect ran')
-    console.log(name)
-    // an empty array as a second argument means that the effect will only run once
-    // if you want to run it every time the name changes, you can add name to the array
-  }, [name])
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+        .then(res => {
+          return res.json()
+        })
+        .then(data => {
+          setBlogs(data)
+          setIsLoading(false)
+        })
+    }, 1000)
+  }, [])
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title = 'All blogs' handleDelete={handleDelete} />
-      {/* get only mario's blogs */}
-      <BlogList blogs={blogs.filter((blog) => blog.author === 'mario' )} title = "Mario's blogs" handleDelete={handleDelete} />
-      {/* get only yoshi's blogs */}
-      <BlogList blogs={blogs.filter((blog) => blog.author === 'yoshi' )} title = "Yoshi's blogs" handleDelete={handleDelete} />
-      <button onClick={() => setName('Anna')}>Change name</button>
-      <p>{name}</p>
+      {isLoading && <div>Loading...</div>}
+      {/* the thing on the right will be output once the left is true */}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   );
 }
