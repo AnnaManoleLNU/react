19c82317cc4be092c351c5ec7889955d6932d19b
Anna Manole <am224wd@student.lnu.se>
2023-03-30 11:37:20 +0200

    dependecies in useEffect

--- my-first-react-app/src/Home.js
+++ my-first-react-app/src/Home.js
@@ -8,6 +8,8 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
 
+  const [name, setName] = useState('mario') // mario is the initial value
+
   const handleDelete = (id) => { 
     const newBlogs = blogs.filter(blog => blog.id !== id)
     setBlogs(newBlogs)
@@ -19,8 +21,10 @@ const Home = () => {
   // use to fetch data from the server
   useEffect(() => {
     console.log('use effect ran')
-    console.log(blogs)
-  })
+    console.log(name)
+    // an empty array as a second argument means that the effect will only run once
+    // if you want to run it every time the name changes, you can add name to the array
+  }, [name])
 
   return (
     <div className="home">
@@ -29,6 +33,8 @@ const Home = () => {
       <BlogList blogs={blogs.filter((blog) => blog.author === 'mario' )} title = "Mario's blogs" handleDelete={handleDelete} />
       {/* get only yoshi's blogs */}
       <BlogList blogs={blogs.filter((blog) => blog.author === 'yoshi' )} title = "Yoshi's blogs" handleDelete={handleDelete} />
+      <button onClick={() => setName('Anna')}>Change name</button>
+      <p>{name}</p>
     </div>
   );
 }
