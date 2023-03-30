d870df7a781ff2d628715c0816552daa75dd5b0a
Anna Manole <am224wd@student.lnu.se>
2023-03-30 14:06:09 +0200

    Throw and render some errors

--- my-first-react-app/src/Home.js
+++ my-first-react-app/src/Home.js
@@ -4,6 +4,7 @@ import BlogList from "./BlogList"
 const Home = () => {
   const [blogs, setBlogs] = useState(null)
   const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState(null)
 
   // use data from the server or communicate with the server in here
   // runs for every render
@@ -13,10 +14,18 @@ const Home = () => {
     setTimeout(() => {
       fetch('http://localhost:8000/blogs')
         .then(res => {
+          if (!res.ok) {
+            throw Error('could not fetch the data for that resource')
+          }
           return res.json()
         })
         .then(data => {
           setBlogs(data)
+          setIsLoading(false)
+          setError(null)
+        })
+        .catch(err => { 
+          setError(err.message)
           setIsLoading(false)
         })
     }, 1000)
@@ -27,6 +36,7 @@ const Home = () => {
       {isLoading && <div>Loading...</div>}
       {/* the thing on the right will be output once the left is true */}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+      {error && <div>{error}</div>}
     </div>
   );
 }
