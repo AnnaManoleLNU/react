60151addc81a4047ee2f0d37bba4a30d5e89340a
Anna Manole <am224wd@student.lnu.se>
2023-03-30 11:03:49 +0200

    Reusable components

--- my-first-react-app/src/Home.js
+++ my-first-react-app/src/Home.js
@@ -1,30 +1,19 @@
 import { useState } from "react"
+import BlogList from "./BlogList"
 
 const Home = () => {
-  // when you need a reactive value you use state hook, you give it an initial value
-  const [name, setName] = useState('Anna')
-  const [age, setAge] = useState('30') // 30 is the initial value
-  // the first parameter is automatically the event parameter, even without writing it
-  const handleClick = (e) => {
-    setName('Cooler Anna') // set it to a new value
-    setAge(31) // set it to a new value
-  }
-  const handleClickAgain = (name, e) => {
-    console.log('hello ' + name, e.target)
-  }
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
 
-  // The use hook provides us with a reactive value and change that value
-  return ( 
+  return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>{name} is {age} years old</p>
-      <button onClick={handleClick}>Click me</button>
-       {/* anon function, if you pass the argument directly you invoke it */}
-       {/* here the anon function gets access to the event object, if you want to use it pass it in as an argument, order does not matter */}
-      <button onClick={(e) => handleClickAgain('Anna', e)}>Click me again</button>
+      <BlogList blogs={blogs} title = 'All blogs!' />
     </div>
-   );
+  );
 }
- 
+
 
 export default Home;
