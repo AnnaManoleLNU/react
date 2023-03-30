8db7a8498a7712e7c46400450e501b71a58fc182
Anna Manole <am224wd@student.lnu.se>
2023-03-30 16:52:42 +0200

    post blogs

--- my-first-react-app/src/Home.js
+++ my-first-react-app/src/Home.js
@@ -12,6 +12,9 @@ const {data, isLoading, error} = useFetch('http://localhost:8000/blogs')
       {/* the thing on the right will be output once the left is true */}
       {data && <BlogList blogs={data} title="All Blogs!" />}
       {error && <div>{error}</div>}
+
+      {data && <BlogList blogs={data.filter((blog) => blog.author === 'yoshi')} title="Yoshi's Blogs" />}
+      
     </div>
   );
 }
