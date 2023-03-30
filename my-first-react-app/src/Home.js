1acd25cd112009c8fe2f943486b624f8573b175f
Anna Manole <am224wd@student.lnu.se>
2023-03-30 10:01:54 +0200

    Click event listeners

--- my-first-react-app/src/Home.js
+++ my-first-react-app/src/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
+
+  // the first parameter is automatically the event parameter, even without writing it
+  const handleClick = (e) => {
+    console.log('I have been clicked', e)
+  }
+  const handleClickAgain = (name, e) => {
+    console.log('hello ' + name, e.target)
+  }
+
   return ( 
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+       {/* anon function, if you pass the argument directly you invoke it */}
+       {/* here the anon function gets access to the event object, if you want to use it pass it in as an argument, order does not matter */}
+      <button onClick={(e) => handleClickAgain('Anna', e)}>Click me again</button>
     </div>
    );
 }
