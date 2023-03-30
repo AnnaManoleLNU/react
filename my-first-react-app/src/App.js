81aa5e5f95e5792887c4d92230e558f6f1ab245e
Anna Manole <am224wd@student.lnu.se>
2023-03-30 17:01:19 +0200

    Add a 404 page

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import Create from './Create';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 // has to start with a capital letter
 // not a function, but a component
@@ -24,6 +25,9 @@ function App() {
             </Route>
             <Route exact path="/blogs/:id">
               <BlogDetails/>
+            </Route>
+            <Route path="*">
+              <NotFound/>
             </Route>
           </Switch>
         </div>
